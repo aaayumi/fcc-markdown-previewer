66c518e2ca1dbd9fc6c7c38ba9a46b2153b49897
Ayumi Saito <ayumisaito27@gmail.com>
2017-05-11 22:03:52 +0200

    upload

--- js/app.js
+++ js/app.js
@@ -1,21 +1,54 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-
 var App = React.createClass({
-    render: function () {
-        return (
-            <div className="todoListMain">
-          <div className="header">
-          <h1> Markdown Previewer</h1>
-            <form>
-              <input>
-              </input>
-            </form>
-          </div>
-        </div>
-        );
-    }
-});
+	getInitialState: function() {
+		return {
+			content: 'Try Markdown here!'
+		}
+	},
+	handleChange: function(event) {
+		this.setState({
+			content: event.target.value
+		})
+	},
+	rawMarkup: function() {
+		marked.setOptions({
+			renderer: new marked.Renderer(),
+			gfm: true,
+			tables: true,
+			breaks: false,
+			pedantic: false,
+			sanitize: true,
+			smartLists: true,
+			smartypants: false,
+			
+		})
 
-ReactDOM.render(<App/>,  document.getElementById("app"));
+		var rawMarkup = marked(this.state.content, {sanitize: true})
+		return {
+			__html: rawMarkup
+		}
+	},
+	
+	render: function() {
+		return (
+			<div className="container-fluid">
+				<div className="row">
+					
+					<div className="col-xs-12 col-sm-6">
+						<h3 id="mark">Markdown Editor</h3>
+						<textarea className="markdown" defaultValue={this.state.content} onChange={this.handleChange}></textarea>
+					</div>
+					<div className="col-xs-12 col-sm-6">
+						<h3>Preview</h3>
+						<div className="preview" dangerouslySetInnerHTML={this.rawMarkup()}>
+                        </div>
+				    </div>
+                </div>
+            </div>
+		)
+	}
+})
+
+ReactDOM.render(
+	<App />, 
+	document.getElementById('app')
+)
